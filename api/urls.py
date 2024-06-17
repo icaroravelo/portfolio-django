507ce60143207f6c1384814372f8c81d8e5175e6
Ícaro Santos <107354899+icaroravelo@users.noreply.github.com>
2024-06-17 20:12:58 +0100

    chore> navbar and vercel settings static and media files

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from .views import testPageView, homePageView, aboutPageView, categoriesPageView, schoolPageView, contactPageView, blogHomePageView
 
@@ -19,3 +21,5 @@ urlpatterns = [
     path("__reload__/", include("django_browser_reload.urls")),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
