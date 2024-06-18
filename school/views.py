b5dd0e35c5e43c4c8f55b72cdafc744967fe14ce
Ícaro Santos <107354899+icaroravelo@users.noreply.github.com>
2024-06-18 19:47:37 +0100

    design layout inside pages

--- school/views.py
+++ school/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from .models import School
 
 # Create your views here.
-def schoolPage(request):
-    schools = School.objects.all() 
+def schoolPage(request, order_by='finished_at'):
+    schools = School.objects.all().order_by(order_by) 
     context = {
-        'schools': schools
+        'schools': schools,
     }
     return render(request, 'school/school.html', context)
